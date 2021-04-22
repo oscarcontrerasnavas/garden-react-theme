3ff0de23a8d0ef391f11a0872c5de3ec22d0de6b
= <=>
2021-04-22 15:51:42 -0500

    Add Newsletter and blog sections to Home

---
+++ src/components/Newsletter.js
@@ -0,0 +1,39 @@
+const Newsletter = () => {
+  return (
+    <div className="newsletter my-5">
+      <div className="container">
+        <div className="row align-items-center justify-content-evenly">
+          <div className="col-12 col-md-3">
+            <p className="fs-1 fw-light mb-0 pb-0">
+              Subscribe to our Newsletter
+            </p>
+          </div>
+          <div className="col-12 col-md-7">
+            <form action="" className="row">
+              <div className="col-9 col-xl-8 position-relative">
+                <label htmlFor="email" className="visually-hidden">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control"
+                  required
+                />
+                <i className="bi bi-envelope fs-5 position-absolute"></i>
+              </div>
+              <div className="col">
+                <button type="submit" className="btn btn-primary text-light">
+                  I want to subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Newsletter;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,48 @@
 import ProductsContainer from "../features/products/ProductsContainer";
+import Newsletter from "../components/Newsletter";
+import blog1 from "../assets/images/blog1.jpg";
+import blog2 from "../assets/images/blog2.jpg";
 
 const Home = () => {
   return (
     <div className="home-page">
       <ProductsContainer />
+      <Newsletter />
+      <div className="container">
+        <h1 className="text-center pt-5">Our Blog</h1>
+        <h4 className="text-center text-primary">~ Latest Entrance ~</h4>
+
+        <div className="row my-5 pt-5">
+          <div className="col-6">
+            <img src={blog1} alt="blog" className="img-fluid" />
+            <h4 className="text-primary mt-4">Jul 15, 2020</h4>
+            <h2 className="fs-5 mb-4">
+              How nature can help building community
+            </h2>
+            <p>
+              Coloring book seitan raclette, literally put a bird on it irony
+              90's. Gochujang actually iPhone butcher cold-pressed paleo.
+              Bushwick chia bespoke listicle tumeric biodiesel portland selfies
+              actually lomo brooklyn.
+            </p>
+            <button className="btn btn-dark mt-3">Read More</button>
+          </div>
+          <div className="col-6">
+            <img src={blog2} alt="blog" className="img-fluid" />
+            <h4 className="text-primary mt-4">Jul 15, 2020</h4>
+            <h2 className="fs-5 mb-4">
+              Meet Rebeca, the owner of this gorgeous lavander farm
+            </h2>
+            <p>
+              Coloring book seitan raclette, literally put a bird on it irony
+              90's. Gochujang actually iPhone butcher cold-pressed paleo.
+              Bushwick chia bespoke listicle tumeric biodiesel portland selfies
+              actually lomo brooklyn.
+            </p>
+            <button className="btn btn-dark mt-3">Read More</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
